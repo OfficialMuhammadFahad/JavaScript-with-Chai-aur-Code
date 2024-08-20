75fae0101f5d483fe5c788d68984e6c01e5b0036
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-20 12:44:17 +0500

    Datatype file added

--- variables.js
+++ variables.js
@@ -1,1 +1,17 @@
-console.log("Hey Boy!")
+var firstName = "Muhammad"
+let lastName = "Fahad"
+const idCardNumber = 4210164666665
+let address;
+let martialStatus;
+
+
+firstName = "Fahad"
+lastName = "Muhammad"
+martialStatus = "Single"
+//idCardNumber = 42101 // const never change
+
+
+console.table([firstName, lastName, idCardNumber, address, martialStatus ]);
+
+
+// Don't use var because of the issues in block scoop and functional scoop  
