416d7c167c0c436d7ffb9e4cf741ad44e99933a6
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-20 15:20:46 +0500

    Datatypes Conversion work added

---
+++ datatypes-conversion.js
@@ -0,0 +1,26 @@
+// Datatype Conversion method
+// For number to boolean
+let isLoggedIn = 1;
+console.log(typeof isLoggedIn);
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+
+
+// For string to number
+let naMe = "Muhammad Fahad"
+console.log(typeof naMe);
+
+let numberName = Number(naMe)
+console.log(typeof numberName);
+
+
+// For number to string
+let num = 123
+console.log(typeof num);
+
+let stringNum = String(num)
+console.log(typeof stringNum);
+
+
