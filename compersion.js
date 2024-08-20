65599f8a601768e1f873405d5a72bda273595aa6
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-20 16:23:31 +0500

    Comparsion work added

---
+++ compersion.js
@@ -0,0 +1,15 @@
+
+console.log(101 < 105);
+console.log(101 > 105);
+console.log(101 <= 105);
+console.log(101 >= 105);
+console.log(101 == 105);
+console.log(101 != 105);
+console.log(101 || 105);
+
+
+
+// Strict check ===
+console.log("101" === 105);
+console.log(101 === 105);
+console.log(105 === 105);
