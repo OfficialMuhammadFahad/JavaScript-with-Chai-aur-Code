3b66a083fde5f56047f4953cec04d38f3a87bfd0
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-20 14:54:35 +0500

    New changes in DataTypes file added

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,28 @@
-console.log("Hey Dude!");
+"use strict"; //Through this line your editor treat JS code as a newer version code
+
+// alert(4 + 4); //This is only browser support not node.js support
+
+// console.log(
+//     3 + 
+//     3
+// );  That's is not a good practice, Code readability is high priority
+
+// Data Types
+// number  =  12, 201, 0
+// string = "ABC"
+// boolean = true/false
+// bigint = the number which is too high 
+// null = null is standalone value
+// undefined = something which is not defined
+// symbol = unique
+// object -->  null is an object datatype
+
+
+
+console.log(typeof 8734); //It's show the datatype of which we give in it
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
