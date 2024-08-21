5612582fb04cdba6bbd6453fcec6a5c0bef242e6
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-21 11:15:32 +0500

    Stack and Heap Memory work added

---
+++ Stack-and-Heap-Memory.js
@@ -0,0 +1,16 @@
+// Stack Memory and Heap Memory
+
+//Stack Memory
+
+//Primitive
+//When we use stack memory so we got the copy of that memory 
+//Use LIFO method
+
+
+
+
+//Heap Memory
+//Non-Primitive
+//When we use heap memory so we got reference of that memory
+//Stay for long term in memory
+
