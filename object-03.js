a4474dc6470fc1cdb80f67bb431264d0bbb54322
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-29 16:20:23 +0500

    Object De-Structure work added

---
+++ object-03.js
@@ -0,0 +1,9 @@
+const depart = {
+    department: "BS Management and Technology",
+    course: "Business Research Methodology",
+    courseInstructor: "Sir Yasir Jamal"
+}
+
+const {courseInstructor: instructor} = depart//That's another way to write array and also change the keys names also called object de-structure
+
+console.log(instructor);
